32e787d36338a05365245d52d5e22a29b5f0c7c0
Yuki Kuroda <yuki.kuroda@supership.jp>
2017-07-31 19:02:31 +0900

    fix response before timeout error occurred

--- window-fetch-retry.js
+++ window-fetch-retry.js
@@ -4,31 +4,38 @@ let _url = '';
 let _options = {};
 let _retryDelay = 0;
 let _timeout = 0;
+
 let _isTimeout = false;
+let _isResolve = false;
 let _retryTask = null;
 
-function retringFetch(retries) {
-  return new Promise((resolve, reject) => {
-    let timeoutId;
-    if (_timeout > 0) {
-      timeoutId = setTimeout(() => {
-        _isTimeout = true;
-        if (_retryTask) clearTimeout(_retryTask);
-        reject(new Error('promise timeout'));
-      }, _timeout);
-    }
-    fetch(_url, _options)
-      .then((res) => {
-        if (!res.ok && retries > 0 && !_isTimeout) {
-          _retryTask = setTimeout(() => {
-            retringFetch(retries - 1);
-          }, _retryDelay);
-          return;
-        }
-        if (timeoutId) clearTimeout(timeoutId);
-        resolve(res);
-      });
-  });
+function setTimeoutAllProcesses(reject) {
+  let timeoutId;
+  if (_timeout > 0) {
+    timeoutId = setTimeout(() => {
+      if (_isResolve) {
+        clearTimeout(timeoutId);
+        return;
+      }
+      _isTimeout = true;
+      if (_retryTask) clearTimeout(_retryTask);
+      reject(new Error('fetch timeout'));
+    }, _timeout);
+  }
+}
+
+function retringFetch(retries, resolve) {
+  fetch(_url, _options)
+    .then((res) => {
+      if (!res.ok && retries > 0 && !_isTimeout) {
+        _retryTask = setTimeout(() => {
+          retringFetch(retries - 1, resolve);
+        }, _retryDelay);
+        return;
+      }
+      _isResolve = true;
+      resolve(res);
+    });
 }
 
 function wrappedFetch(url, options) {
@@ -41,7 +48,11 @@ function wrappedFetch(url, options) {
   delete _options.retries;
   delete _options.retryDelay;
   delete _options.timeout;
-  return retringFetch(retries);
+
+  return new Promise((resolve, reject) => {
+    setTimeoutAllProcesses(reject);
+    retringFetch(retries, resolve);
+  });
 }
 
 export default wrappedFetch;
